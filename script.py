6ae44dcaba54984b0f940623cc713f9ef727276a
Matias Carabella <matias.carabella@outlook.com>
2023-03-12 01:13:49 -0300

    Further improvements, adding constants

--- script.py
+++ script.py
@@ -18,7 +18,7 @@ def split_names(string):
     # Define a regular expression pattern using a lookbehind assertion (?<=)
     # and a lookahead assertion (?=). This will match any position between
     # a lowercase letter and an uppercase letter.
-    pattern = r"(?<=[a-z])(?=[A-Z])"
+    pattern = r"(?<=[a-z.])(?=[A-Z])"
     
     # Use the re.split() function to split the string using the regular
     # expression pattern as the delimiter.
@@ -35,11 +35,29 @@ def clean_constructor_name(constructor):
 
     return constructor
 
-# Set the year to retrieve data for
-year = 2014
+### CONSTANTS ### 
+# You'll probably have to modify some of these depending on the YEAR you are querying
+
+YEAR = 1990 # Year from which to retrieve data
+
+ENTRIES_TABLE_INDEX = 1 # USUALLY '0' OR '1'
+RESULTS_TABLE_INDEX = 5 # USUALLY '4', '5', OR '6'
+# When running the script, a 'tables.html' file is generated. If one of them (or both) are not the right ones, then modify these.
+
+ENTRIES_START_FROM_INDEX = 1 # USUALLY '1'
+RESULTS_START_FROM_INDEX = 2 # USUALLY '1' OR '2'
+# These are for ignoring the header rows, the number should match the first row with actual data.
+
+SPECIAL_FORMAT = YEAR in [2015, 2017, 2018, 2019, 2020, 2021, 2022]
+# Sometimes there's NO headers in the Entries table, this boolean takes that into account.
+
+ENTRIES_DRIVER_MAIN_INDEX = 5 # USUALLY EITHER '4' OR '5'
+ENTRIES_DRIVER_SECONDARY_INDEX = 0 # USUALLY EITHER '0' OR '1'
+# The column index in which the Driver name is present. Given that the Constructor spans multiple Drivers (but only counts as a column in its first row),
+# the first driver from a given team is always a higher index than the rest.
 
 # Define the URL of the Wikipedia page to retrieve data from
-url = 'https://en.wikipedia.org/wiki/' + str(year) + '_Formula_One_World_Championship'
+url = 'https://en.wikipedia.org/wiki/' + str(YEAR) + '_Formula_One_World_Championship'
 
 # Send an HTTP request to the webpage and retrieve the HTML content
 response = requests.get(url)
@@ -50,8 +68,8 @@ soup = BeautifulSoup(html_content, 'html.parser')
 tables = soup.find_all('table')
 
 # Extract the tables for entries and final standings
-table_entries = tables[0] # USUALLY '0' OR '1'
-table_results = tables[6] # USUALLY '4', '5', OR '6'
+table_entries = tables[ENTRIES_TABLE_INDEX] # USUALLY '0' OR '1'
+table_results = tables[RESULTS_TABLE_INDEX] # USUALLY '4', '5', OR '6'
 
 ### DEBUG ###
 
@@ -69,7 +87,7 @@ with open(filename, 'w') as f:
 ### ENTRIES TABLE ###
 
 # Extract the rows from the entries table (skipping the first row which contains the headers)
-rows = table_entries.find_all('tr')[1:]
+rows = table_entries.find_all('tr')[ENTRIES_START_FROM_INDEX:]
 
 # Initialize the most recent value of the Constructor column to an empty string
 latest_constructor = ''
@@ -88,19 +106,26 @@ for row in rows:
             break
         latest_constructor = constructor
         if (cols_td):
-            drivers = cols_td[4] # USUALLY EITHER '4' OR '5'
+            if (len(cols_td) < 6): # This takes an edge case (combined cells) into account
+                continue
+            drivers = cols_td[ENTRIES_DRIVER_MAIN_INDEX] # USUALLY EITHER '4' OR '5'
     else:
-        latest_constructor = constructor
-        # If the Driver column does not contain letters, then it's not really the Driver column, so we look for it at a different index
-        if re.search(r'[A-Za-z]', cols_td[0].get_text(strip=True)): # USUALLY EITHER '0' OR '1'
-            drivers = cols_td[0] # USUALLY EITHER '0' OR '1'
+        if SPECIAL_FORMAT:
+            if (clean_constructor_name(cols_td[0]) == 'Sources:'):
+                break
+            constructor = clean_constructor_name(cols_td[1])
+            drivers = cols_td[5]
         else:
-            drivers = cols_td[1] # USUALLY EITHER '0' OR '1'
-
+            latest_constructor = constructor
+            # If the Driver column does not contain letters, then it's not really the Driver column, so we look for it at a different index
+            if re.search(r'[A-Za-z]', cols_td[ENTRIES_DRIVER_SECONDARY_INDEX].get_text(strip=True)): # USUALLY EITHER '0' OR '1'
+                drivers = cols_td[ENTRIES_DRIVER_SECONDARY_INDEX] # USUALLY EITHER '0' OR '1'
+            else:
+                drivers = cols_td[ENTRIES_DRIVER_SECONDARY_INDEX + 1] # USUALLY EITHER '0' OR '1'
     for driver in split_names(clean_driver_name(drivers.get_text(strip=True))):
         # Append the extracted data to the list of dictionaries
         data.append({
-            'year': year, 
+            'year': YEAR, 
             'name': driver, 
             'team': constructor})
 
@@ -111,7 +136,7 @@ with open('entry_output.json', 'w') as f:
 ### FINAL STANDINGS TABLE ###
 
 # Extract the rows from the final standings table (skipping the first two rows which contain the headers)
-rows = table_results.find_all('tr')[1:] # USUALLY '1' OR '2'
+rows = table_results.find_all('tr')[RESULTS_START_FROM_INDEX:] # USUALLY '1' OR '2'
 
 # Extract the values of the Driver, Championship Position, Points, Wins, and Podiums columns from each row and store them in a list of dictionaries
 data = []
@@ -125,22 +150,35 @@ for row in rows:
     cols_th = row.find_all('th')
     cols_td = row.find_all('td')
 
+    if (len(cols_th) == 0):
+        break
+
     # Extract the Championship Position, Points and Driver columns. If Championship Position equals 'Pos.' then that's the end of the table so we end the loop
     championship_position = cols_th[0].get_text(strip=True)
     if championship_position == 'Pos.':
         break
-    points = cols_th[1].get_text(strip=True)
+
+    # SOMETIMES INDEX '1' DOESN'T EXIST, AND POINTS ARE PRESENT IN THE FINAL <td>
+    if (len(cols_th)) > 1: 
+        points = cols_th[1].get_text(strip=True) 
+        points_header = True
+    else:
+        points = cols_td[len(cols_td) - 1].get_text(strip=True)
+        points_header = False
+
     driver = clean_driver_name(cols_td[0].get_text(strip=True))
 
     # Extract the Wins and Podiums columns
-    for cols in cols_td[1:]:
+    for i, cols in enumerate(cols_td[1:]):
+        if (not(points_header) and i == len(cols_td) - 2):
+            break
         race_position = cols.text.strip()
         match = re.search(r'\d+', race_position)
         if match:
             race_position = int(match.group())
             if race_position == 1:
                 wins += 1
-            if race_position <= 3:
+            if race_position <= 3 and race_position != 0: # Beware drivers with exactly '1' point, it might count as a Win/Podium
                 podiums += 1
     
     # Append the extracted data to the list of dictionaries
